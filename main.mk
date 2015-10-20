0b6d22aa2691436c6e52894d8b147bf2b3bc42e6
Carsten Munk <carsten.munk@jollamobile.com>
2015-10-20 09:07:32 +0200

    Add klaatu aidl_cpp
    Signed-off-by: Carsten Munk <carsten.munk@jollamobile.com>

--- main.mk
+++ main.mk
@@ -2,9 +2,9 @@
 
 CUTEBOOT_BUILD_TOP=$(PWD)
 
-all: sysroot subdirs qmlscene
+all: sysroot subdirs qmlscene bin/aidl_cpp
 
-SUBDIRS = qtbase qtdeclarative qtmultimedia aidl
+SUBDIRS = qtbase qtdeclarative qtmultimedia
 
 .PHONY: subdirs $(SUBDIRS)
 
@@ -80,33 +80,12 @@ configure-eglfs_surfaceflinger: qtbase
 
 hwdep: eglfs_surfaceflinger
 
-aidl:
-	cd aidl ; \
+bin/aidl_cpp:
+	cd aidl_cpp ; \
 	$(PWD)/prebuilts/misc/linux-x86/flex/flex-2.5.39 aidl_language_l.l ; \
 	$(PWD)/prebuilts/misc/linux-x86/bison/bison --defines=aidl_language_y.hpp --output=aidl_language_y.cpp aidl_language_y.y ; \
-	$(PWD)/prebuilts/clang/linux-x86/host/3.5/bin/clang++ -o aidl_cpp \
-			aidl.cpp \
-			aidl_language.cpp \
-			aidl_language_y.cpp \
-			ast_cpp.cpp \
-			ast_java.cpp \
-			code_writer.cpp \
-			generate_cpp.cpp \
-			generate_java.cpp \
-			generate_java_binder.cpp \
-			import_resolver.cpp \
-			io_delegate.cpp \
-			options.cpp \
-			type_cpp.cpp \
-			type_java.cpp \
-			type_namespace.cpp \
-			-Iexternal/gtest/include \
-			-Ibase/include -I. -std=c++11 \
-			base/logging.cpp \
-			base/stringprintf.cpp \
-			base/strings.cpp \
-			lex.yy.c \
-			main_cpp.cpp ; \
+	g++ -o aidl_cpp \
+			*.cpp lex.yy.c -I. ; \
 	mkdir -p $(PWD)/bin ; \
 	cp aidl_cpp $(PWD)/bin
 
